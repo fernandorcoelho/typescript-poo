d8fc84221cd72ff1f48fd4e1435d585097b8d15a
Fernando <fernando.ufv14@gmail.com>
2025-05-20 21:13:06 -0300

    Encapsulamento - Modificadores de Acesso

--- index.ts
+++ index.ts
@@ -3,7 +3,16 @@ import { Person } from "./banking/Person";
 function main() {
   const maria = new Person("Maria", "da Silva", new Date("1999-01-15"));
 
-  console.log(maria);
+  console.log(maria.getFullName());
+
+  maria.updateFirstName("Maria Clara");
+
+  maria.getFullName();
+
+  //Property isBirthdayPassed is protected and only accessible within class  and its subclasses.
+  // maria.isBirthdayPassed();
+
+  console.log(maria.getFullName());
 }
 
 main();
