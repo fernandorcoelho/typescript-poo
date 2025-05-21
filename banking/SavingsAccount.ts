23d9eae4f8ba9e683ecf9a1c8d205530341d298e
Fernando <63662083+fernandorcoelho@users.noreply.github.com>
2025-05-21 16:25:24 -0300

    Polimorfismo na prática

--- banking/SavingsAccount.ts
+++ banking/SavingsAccount.ts
@@ -4,7 +4,7 @@ import { Person } from "./Person";
 // subclass
 export class SavingsAccount extends BankAccount {
   private interestRate: number; // taxa de juros
-  
+
   constructor(
     person: Person,
     initialBalance: number,
@@ -14,6 +14,10 @@ export class SavingsAccount extends BankAccount {
     super(person, initialBalance, accountNumber);
     this.interestRate = interestRate;
   }
-}
 
-export class CurrentAccount extends BankAccount {}
+  addInterest(): void {
+    const interest = this.balance * (this.interestRate / 100);
+    this.deposit(interest);
+    console.log(`Interest added: $${interest}. New Balance: $${this.balance}`);
+  }
+}
